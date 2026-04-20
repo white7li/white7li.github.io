367b479f1cd675a839c68efdacef6662c3d692ef
white7li <2979424069@qq.com>
2026-04-20 21:18:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+let score = 0;
+
+function update() {
+    document.getElementById("score").innerText = score;
+
+    let status = "";
+
+    if (score >= 10) {
+        status = "🔥 很强";
+    } else if (score >= 5) {
+        status = "🙂 不错";
+    } else if (score >= 0) {
+        status = "🙂 继续加油";
+    } else {
+        status = "💀 负分了";
+    }
+
+    document.getElementById("status").innerText = status;
+}
+
 function add() {
-    num++;
-    document.getElementById("count").innerText = num;
+    score++;
+    update();
+}
+
+function minus() {
+    score--;
+    update();
 }
